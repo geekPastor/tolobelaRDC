64a2ccca5c93feaf11afdfcc735bd3da4767b553
= <=>
2023-06-07 17:28:10 +0200

    [register] add register

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,10 +3,15 @@ import {
   } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
     {
       path: "/login",
       element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
     },
   ]);
--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -8,13 +8,13 @@ import logo from "../assets/logo.jpg";
 import "../Sass/LoginComponent.scss"
 import { toast } from "react-toastify";
 
-export default function LoginComponent(){
+export default function RegisterComponent(){
     let navigate = useNavigate()
     const [credentails, setCredentails] = useState({});
     const login = async ()=>{
        try {
         let res = await LoginAPI(credentails.email, credentails.password);
-        toast.success('Bon retour sur Tolobela-RDC, content de vous revoir');
+        toast.success('Bienvenu sur Tolobela-RDC, votre compte a bien été créé !');
        } catch (err) {
         toast.error("S'il vous plait verifier bien vos informations, ou inscrivez-vous d'abord");
        }
@@ -25,13 +25,13 @@ export default function LoginComponent(){
     }
     return(
         <div className="flex flex-col md:flex-row h-screen items-center">
-            <div className="h-screen md:w-1/2 xl:w-2/3 lg-block">
+            <div className="h-screen md:w-1/2 xl:w-1.5/3 lg-block">
                 <img src={logo} alt=""  className="w-full h-full object"/>
             </div>
-            <div className="bg-white items-center justify-center flex md:mx-auto md:mx-0 md:max-w-md lg:max-w-full  w-full md:w-1/2 xl:w-1/3 px-6 lg:px-16 xl:px-12">
+            <div className="bg-white items-center justify-center flex md:mx-auto md:mx-0 md:max-w-md lg:max-w-full  w-full md:w-1/2 xl:w-1.1/3 px-6 lg:px-16 xl:px-12">
                 <div className="w-full h-100">
                     <h1 className="text-2xl uppercase text-blue-500 font-bold">tolobela-RDC</h1>
-                    <p className="text-xs md:text-2xl  leading-tightmt-12">Connectez-vous à votre compte</p>
+                    <p className="text-xs md:text-2xl  leading-tightmt-12">Creer votre compte pour qu'ensemble nous parlions de la RDC</p>
 
                     <div className="mt-6">
                         <div>
@@ -49,13 +49,9 @@ export default function LoginComponent(){
                                 type="password" className="w-full bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none rounded-lg px-4 py-3"
                                 placeholder="Entrez votre Mot de passe"/>
                         </div>
-
-                        <div className="text-right mt-2">
-                            <a href="#" className="text-sm font-semibold text-gray-700">Forgot password </a>
-                        </div>
-
-                        <button onClick={login} className="w-full block bg-blue-500 hover:bg-blue-400 px-4 py-3 mt-6 rounded-lg font-semibold text-white focus:bg-blue-400 focus:outline-none">
-                            Se connecter
+                            <p className="mt-3">En cliquant sur s'inscrire vous acceptez notre politique de <span className="text-blue-500 hover:cursor-pointer">confidentialité</span></p>
+                        <button onClick={login} className="w-full block bg-blue-500 hover:bg-blue-400 px-4 py-3 mt-4 rounded-lg font-semibold text-white focus:bg-blue-400 focus:outline-none">
+                            S'inscrire
                         </button>
 
                         <hr className="my-6 border-gray-300 w-full"/>
@@ -67,7 +63,7 @@ export default function LoginComponent(){
                         
 
                         <p className="mt-4">
-                            Vous n'avez pas de compte ? <span className="text-blue-500 hover:cursor-pointer" onClick={()=> navigate('/register')}>Inscrivez-vous</span>
+                            Vous avez déjà un compte ? <span className="text-blue-500 hover:cursor-pointer" onClick={()=> navigate('/login')}>connectez-vous</span>
                         </p>
                     </div>
                 </div>
