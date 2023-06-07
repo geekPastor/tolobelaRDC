4f3543385a9efc0d69f3cfc044da9704be3bc8ea
= <=>
2023-06-07 22:04:05 +0200

    [Authentification] add cookies management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, } from "react";
 import HomeComponent from "../components/HomeComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home(){
+    let navigate = useNavigate();
+    useEffect(()=>{
+        onAuthStateChanged(auth, (res)=>{
+            if(!res?.accessToken){
+                navigate("/login")
+            }
+        })
+    }, []);
+
     return(
         <div>
             <HomeComponent />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 import LoginComponent from "../components/LoginComponent";
-
+import { useNavigate } from "react-router-dom";
 export default function Login(){
+    let navigate = useNavigate();
+    useEffect(()=>{
+        onAuthStateChanged(auth, (res)=>{
+            if(res?.accessToken){
+                navigate("/home")
+            }
+        })
+    }, []);
     return(
         <LoginComponent/>
     )
